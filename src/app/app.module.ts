4b708ff37c6a1cac25f270f4d5ab98b6b7c3ae1c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-05 20:18:02 -0300

    Adicionando uma route que varia conforme você clicar em algum dos produtos, direcionando para o mesmo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { ProductAlertsComponent } from './components/product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     TopBarComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
